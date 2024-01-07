40a6eca57001f214c5e7a2d4308e5bf584f47ebd
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2024-01-06 23:40:58 -0800

    Update Footer.js

--- rypto-map/src/pages/Footer.js
+++ rypto-map/src/pages/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return(
-    // gotta include because of 'attribution required' for free tier
+    // gotta include because of 'attribution required' for free tier of BSC Scan API
     <h1>  "Powered by BscScan APIs" </h1> 
     
     )
