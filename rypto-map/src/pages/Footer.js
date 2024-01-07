65c19dc8bf540095ccfde1604f3b8f68703d73af
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2024-01-06 23:15:06 -0800

    Mandatory Attribution for BSC Scan

---
+++ rypto-map/src/pages/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+    // gotta include because of 'attribution required' for free tier
+    <h1>  "Powered by BscScan APIs" </h1> 
+    
+    )
+
+
+}; 
+export default Footer; 
--- rypto-map/src/pages/index.js
+++ rypto-map/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as MapPage} from './MapPage.js'; 
 export { default as CoffeeForm} from './CoffeeForm.js'; 
+export { default as Footer} from './Footer.js'; 
