f202ff147aa47bbff7984108376a24380d936f18
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2024-01-07 17:20:50 -0800

    Working on Context

--- rypto-map/src/App.js
+++ rypto-map/src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 
-import { MapPage, CoffeeForm }from "./pages";
+import {CoffeeForm }from "./pages";
 import { Row, Col} from 'react-bootstrap';
 
 import {useState, React } from 'react'; 
 import { MarkerContext } from './pages/MarkerContext';
 
 function App() {
+  const [markers, setMarkers] = useState([]); 
+
   return (
     <div id = "siteLayout"> 
       <h1> One of Kind: Long Beach Edition! </h1> 
       <p> Connect your metamask wallet, add coffee shops to the map and get rewarded for adding *unique* spots! </p>
-      <Row> 
-        <Col className = "col col-lg-4" > 
-          <CoffeeForm /> 
-        </Col>
-        <Col className = "col"> 
-        < MapPage /> 
-        </Col>
-      </Row>  
+      <div> 
+        <CoffeeForm /> 
+      </div>
     </div> 
   );
 }
--- rypto-map/src/pages/CoffeeForm.js
+++ rypto-map/src/pages/CoffeeForm.js
@@ -1,77 +1,46 @@
 import React, { useState, useContext } from 'react';
 import { Container, Form, Button, Alert } from 'react-bootstrap';
+
 import { MarkersContext } from './MarkersContext';
 import axios from 'axios';
+import { MarkerContext } from './MarkerContext';
 
 
 function CoffeeForm() {
-    // const [address, setAddress] = useState('');
-    // const [error, setError] = useState('');
-    // const [showCityAlert, setCityAlert] = useState(false); // Define both showAlert and setShowAlert
-    // const [cityError, setCityError] = useState('');
-    // const [showAlert, setShowAlert] = useState(false); // Define both showAlert and setShowAlert
-    // const { addMarker } = useContext(MarkersContext);
-
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault();
-    //     setError('');
-    //     setShowAlert(false); // Reset showAlert state
-
-    //     try {
-    //       if (address.includes("Long Beach, CA")) {
-    //         const response = await axios.get(`https://nominatim.openstreetmap.org/search`, {
-    //           params: {
-    //               format: 'json',
-    //               q: address
-    //             }
-    //         }); 
-
-    //       if (response.data && response.data.length > 0) {
-    //           const loc = response.data[0];
-    //           addMarker({
-    //               position: [parseFloat(loc.lat), parseFloat(loc.lon)],
-    //               label: address
-    //           });
-    //           setAddress('');
-    //       } else {
-    //           setCityError('No results found for the given address.');
-    //           setCityAlert(true); // Set showAlert to true if no results found
-    //       }
-    //       } else {
-    //         // Show alert if address is not valid
-    //         setShowAlert(true);
-    //       }
-    //     } catch (err) {
-    //         setError('An error occurred while searching for the address.');
-    //         setShowAlert(true); // Set showAlert to true if an error occurred
-    //     }
-    // };
-
+  const [marker,setMarker] = useState([])
 
   return (
-    <Container className="mt-5">
-      <Form id="addressForm" onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label htmlFor="address">Address</Form.Label>
-          <Form.Control
-            type="text"
-            id="address"
-            placeholder="Enter address in Long Beach, CA"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </Form.Group>
-      <Button variant="primary" type="submit" id = "formSubmitBtn" >
-          Submit
-        </Button>
 
-      </Form>
+    <Row> 
+    <Col className = "col col-lg-4" >
+        <Container className="mt-5">
+        <Form id="addressForm" onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label htmlFor="address">Address</Form.Label>
+            <Form.Control
+              type="text"
+              id="address"
+              placeholder="Enter address in Long Beach, CA"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </Form.Group>
+        <Button variant="primary" type="submit" id = "formSubmitBtn" >
+            Submit
+          </Button>
 
-
-      <Alert  variant="danger" show={showCityAlert} className="mt-3">
-        Address is not valid. Please enter an address in Long Beach, California.
-      </Alert>
-    </Container>
+        </Form>
+        <Alert  variant="danger" show={showCityAlert} className="mt-3">
+          Address is not valid. Please enter an address in Long Beach, California.
+        </Alert>
+      </Container>          
+    <Col className = "col"> 
+    </Col>
+    <MarkerContext.Provider value={{text, setText}}> 
+      < MapPage /> 
+    </MarkerContext.Provider>
+    </Col>
+  </Row>  
   );
 }
 
