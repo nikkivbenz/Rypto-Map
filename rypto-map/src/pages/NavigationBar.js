7ec09ed9695d85dc1390dde7338272778f46ebff
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2024-01-16 23:42:58 -0800

    Update NavigationBar.js

--- rypto-map/src/pages/NavigationBar.js
+++ rypto-map/src/pages/NavigationBar.js
@@ -1,10 +1,18 @@
 import Nav from 'react-bootstrap/Nav';
+import React, { useState } from 'react';
+import { Modal, Button, Card } from 'react-bootstrap';
+
+
 
 function NavigationBar() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => setShowModal(!showModal);
   return (
+    <div> 
     <Nav id="nav" variant="pills" className="justify-content-center" defaultActiveKey="/home">
       <Nav.Item>
-        <Nav.Link eventKey="line-0">what's this for? </Nav.Link>
+        <Nav.Link onClick={toggleModal}>what's this for? </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="link-1">technologies implemented</Nav.Link>
@@ -18,6 +26,29 @@ function NavigationBar() {
 
 
     </Nav>
+
+
+    <Modal show={showModal} onHide={toggleModal}>
+    <Modal.Header closeButton>
+      <Modal.Title>about rypto coffee map!</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Card>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={toggleModal}>
+        Close
+      </Button>
+    </Modal.Footer>
+    </Modal>
+    </div> 
   );
 }
 
